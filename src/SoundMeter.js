d998a0323adb98384d7081c344439a91dfa728e0
dancreed <Dan.Creed@thecreeds.net>
2025-08-29 10:37:03 -0500

    Update SoundMeter.js

--- src/SoundMeter.js
+++ src/SoundMeter.js
@@ -47,7 +47,7 @@ export default function SoundMeter({ listening }) {
         }
         draw();
       } catch {
-        /* fail silently on forbidden */
+        /* fail silently if denied */
       }
     }
 
